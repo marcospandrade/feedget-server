dfaca183b20dec514f029b89bb91bf14aa8eed6a
Marcos Andrade <marcospandrade2@gmail.com>
2022-06-13 18:43:20 -0300

    config prisma

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,26 @@
-import express from 'express'
+import express from 'express';
+import { prisma } from './prisma';
 
 const app = express();
+
+app.use(express.json());
+
 const port = 3333;
 
-app.get("/", (req, res) => {
-    res.send('hello world')
-})
+app.post('/feedbacks', async (req, res) => {
+    const { type, comment, screenshot } = req.body;
+
+    const feedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            screenshot
+        }
+    });
+
+    return res.status(201).json({ data: feedback });
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`)
-})
+    console.log(`Server is running on port: ${port}`);
+});
